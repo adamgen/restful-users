345f16c0ba0c83bc487bebffdfdeab9429639b0d
Adam <adam.genshaft@globality-contractor.com>
2019-10-22 22:57:36 +0300

    Store session in mongo

--- src/init-middleware.ts
+++ src/init-middleware.ts
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import expressSession from 'express-session';
 import passport from 'passport';
+import MongoStore from 'connect-mongo';
+import mongoose from 'mongoose';
+const mongoStore = MongoStore(expressSession);
 
 export function expressPassportSetupMiddleware() {
     return [
@@ -13,6 +16,9 @@ export function expressPassportSetupMiddleware() {
             secret: 'keyboard cat',
             resave: false,
             saveUninitialized: false,
+            store: new mongoStore({
+                mongooseConnection: mongoose.connection
+            }),
         }),
         passport.initialize(),
         passport.session(),
