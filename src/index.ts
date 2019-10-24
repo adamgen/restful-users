6579af298174e4f8595d4dd7164b5f4eed4dfcf3
Adam <adam.genshaft@globality-contractor.com>
2019-10-24 09:26:51 +0300

    Update module exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './config/passport';
 import { expressPassportSetupMiddleware } from './init-middleware';
-export { router  as usersSessionsRouter} from './resources/router';
+export { router as usersSessionsRouter } from './resources/router';
 export const suggestedMiddleware = expressPassportSetupMiddleware;
+export { User } from './resources/users/users.schema';
